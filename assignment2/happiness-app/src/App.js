2689e2a758738de41a495ff54d0cd76edff66c0f
Jack <jack.x.chen@outlook.com>
2023-08-28 22:51:44 +1000

    Added routing to happiness app

--- assignment2/happiness-app/src/App.js
+++ assignment2/happiness-app/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import Register from './pages/Register'
+import Login from "./pages/Login";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<p>Home</p>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
