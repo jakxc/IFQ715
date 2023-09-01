b9f7d26816b45b99cf6d5e18e385f2cac7d83c9f
Jack <jack.x.chen@outlook.com>
2023-09-01 15:32:34 +1000

    Updated home page in portfolio

--- module-6/my-portfolio/src/components/Header/index.js
+++ module-6/my-portfolio/src/components/Header/index.js
@@ -1,34 +1,55 @@
-import { Link, useResolvedPath, useMatch } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Container, Navbar, Nav } from "react-bootstrap";
+import linkedIn from '../../assets/linkedIn-icon.svg';
+import github from '../../assets/github-icon.png';
+import instagram from '../../assets/instagram-icon.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import "./styles.css"
 
 const HighlightLink = (props) => {
-    let resolved = useResolvedPath(props.to);
-    let match = useMatch({ path: resolved.pathname, end: true });
-    return <Nav.Link {...props} active={match} className="primary-color"/>;
+    return <NavLink 
+                {...props}
+                className="fw-bold"
+                style={({ isActive }) => ({
+                    color: isActive ? 'rgb(228, 228, 228)' : 'rgb(224, 136, 92)',
+                  })}
+            />;
 }
 
 const Header = () => {
     return (
         <header>
-            <Navbar expand="md">
-                <Container fluid>
-                    <Navbar.Brand href="#" className="primary-color">Jakxc</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarSupportedContent" />
-                    <Navbar.Collapse id="navbarSupportedContent">
-                        <Nav className="me-auto">
-                            <HighlightLink to="/" as={Link}>
+            <Container fluid className="d-flex justify-content-between p-3">
+                <Navbar expand="md" className="nav | m-0 p-0">
+                    {/* <Navbar.Brand href="#" className="primary-color | fw-bold">Jakxc</Navbar.Brand> */}
+                    <Navbar.Toggle aria-controls="navbarSupportedContent" className="nav_toggle">
+                        <span>
+                            <FontAwesomeIcon
+                                icon={faBars} size="2x"
+                            />
+                        </span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="navbarSupportedContent" className="pt-2">
+                        <Nav className="gap-3 me-auto">
+                            <HighlightLink to="/">
                                 Home
                             </HighlightLink>
-                            <HighlightLink to="/portfolio" as={Link}>
+                            <HighlightLink to="/portfolio">
                                 Portfolio
                             </HighlightLink>
-                            <HighlightLink to="/contact" as={Link}>
-                               Contact Me
+                            <HighlightLink to="/contact">
+                                Contact Me
                             </HighlightLink>
                         </Nav>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                </Navbar>
+                <div className="social-icon | d-flex gap-3">
+                    <a href="https://www.linkedin.com/in/jack-chen-798696196/" className="d-inline-flex justify-content-center align-items-center"><img src={linkedIn} alt="LinkedIn" /></a>
+                    <a href="https://github.com/jakxc/" className="d-inline-flex justify-content-center align-items-center"><img src={github} alt="Github" /></a>
+                    <a href="https://www.instagram.com/ray.jxc/" className="d-inline-flex justify-content-center align-items-center"><img src={instagram} alt="Instagram" /></a>
+                </div>
+            </Container>
         </header>
     )
 }
--- module-6/my-portfolio/src/pages/Home/index.js
+++ module-6/my-portfolio/src/pages/Home/index.js
@@ -1,15 +1,16 @@
 import { Container, Col, Row } from "react-bootstrap";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
+import profileImg from "../../assets/profile.jpg"
 import "./styles.css"
 
 const Home = () => {
     return (
-        <Container fluid="sm" className="mt-5 pt-5">
+        <Container fluid="sm" className="mt-5 py-5">
             <Row>
-                <Col sm={12} md={6} className="gap-2">
+                <Col sm={12} md={8} className="gap-2 mb-3">
                     <h2>
                         Hello, my name is <span className="primary-color | fw-bolder">Jack</span>. 
-                        I am a <span className="primary-color">
+                        I am a <span className="primary-color | fw-bold">
                         <Typewriter
                             options={{
                                 strings: ['Full Stack Web Developer', 'UX Designer', 'Front End Developer'],
@@ -20,9 +21,9 @@ const Home = () => {
                     </h2>
                     <button type="button" class="primary-background text-dark | btn fw-bold">Contact Me</button>
                 </Col>
-                <Col sm={12} md={6}>
+                <Col sm={12} md={4}>
                     <div className="profile-frame | mx-auto">
-                    
+                        <img src={profileImg} className="profile-img" alt="Profile"/>
                     </div>
                 </Col>
             </Row>
