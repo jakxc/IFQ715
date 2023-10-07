c8be5660c14fae904dc03af9abf3c0954fbe7ddf
Jack <jack.x.chen@outlook.com>
2023-10-07 16:41:31 +1000

    Updated ProjectCard

--- module-6/my-portfolio/src/components/Card/index.js
+++ module-6/my-portfolio/src/components/Card/index.js
@@ -4,19 +4,13 @@ import test from "../../assets/profile.jpg"
 
 const ProjectCard = () => {
     return (
-        <div className="card">
-            <div className="card_section thumbnail">
-                <div className="content">
-                    <img src="" />
-                    <h3>Design</h3>
-                </div>
+        <div class="card">
+            <div class="card__content">
+                <p class="card__title">Card Title</p>
+                <p class="card__desc">Card Desc.</p>
+                <button type="button" className="btn-primary card__btn | mt-3">View</button>
             </div>
-            <div className="card_section desc">
-                <div className="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Read More</a>
-                </div>
-            </div>
+            <img src="https://images.unsplash.com/photo-1482877346909-048fb6477632?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=958&q=80" alt="article-cover" />
         </div>
     )
 }
